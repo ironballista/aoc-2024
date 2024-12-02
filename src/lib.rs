4e3dd9b309290ccf9bca575e966474953ef8b8b7
ironballista <87903825+ironballista@users.noreply.github.com>
2024-12-02 19:45:49 +0100

    Non-working solution to day two, part two.

--- src/lib.rs
+++ src/lib.rs
@@ -61,3 +61,59 @@ pub fn day_two_first() -> i64 {
 
     count
 }
+
+pub fn day_two_second() -> i64 {
+    let mut count = 0;
+
+    fn evaluate<I: Iterator<Item = i64> + Clone>(iter: &I) -> bool {
+        iter.clone().all(|diff| (-3..0).contains(&diff))
+            || iter.clone().all(|diff| (1..=3).contains(&diff))
+    }
+
+    'outer: for line in stdin().lines().map(Result::unwrap) {
+        let diffs: Vec<_> = line
+            .split_whitespace()
+            .map(str::parse::<i64>)
+            .map(Result::unwrap)
+            .map_windows(|[l, r]| l - r)
+            .collect();
+
+        let len = diffs.iter().count();
+        let increasing: Vec<_> = diffs.iter().map(|diff| (-3..0).contains(diff)).collect();
+        let decreasing: Vec<_> = diffs.iter().map(|diff| (1..=3).contains(diff)).collect();
+
+        let count_inc = usize::abs_diff(len, increasing.iter().filter(|b| **b).count());
+        let count_dec = usize::abs_diff(len, decreasing.iter().filter(|b| **b).count());
+
+        if count_inc == 0 || count_dec == 0 {
+            count += 1;
+        } else {
+            let errors_inc: Vec<_> = increasing.iter().enumerate().filter(|(_idx, &it)| !it).map(|(idx, _)| idx).collect();
+            let errors_dec: Vec<_> = decreasing.iter().enumerate().filter(|(_idx, &it)| !it).map(|(idx, _)| idx).collect();
+
+            for &idx in errors_inc.iter().chain(errors_dec.iter()) {
+                let mut copy = diffs.clone();
+
+                if idx != 0 && idx != copy.len() - 1 {
+                    copy[idx+1] += copy[idx];
+                }
+
+                copy.remove(idx);
+                if evaluate(&copy.iter().cloned()) {
+                    count += 1;
+                    continue 'outer;
+                } else {
+                    dbg!(&line);
+                    dbg!(&diffs);
+                    dbg!(len);
+                    dbg!(&increasing);
+                    dbg!(&decreasing);
+                    dbg!(&copy);
+                    eprintln!();
+                }
+            }
+        }
+    }
+
+    count
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use aud_aoc_2024::*;
 
 fn main() {
-    println!("{}", day_two_first());
+    println!("{}", day_two_second());
 }
